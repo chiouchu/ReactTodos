5ec5c9649ad99850e38a0407a2abe93518b6ed1f
Chiou Chu <enjoychu@gmail.com>
2016-12-10 18:22:51 +0800

    Fix filter reducer case constant

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes';
 
 export default function filter(state = 'SHOW_ALL', action) {
     switch (action.type) {
-        case 'SET_FILTER':
+        case types.SET_FILTER:
             return action.filter;
         default:
             return state;
